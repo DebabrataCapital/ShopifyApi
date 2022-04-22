f684414c0a513040a40f47939ac46f56dcfec4fe
DebabrataCapital <71883962+DebabrataCapital@users.noreply.github.com>
2022-04-22 16:26:37 +0530

    make it better2

--- index.php
+++ index.php
@@ -4,96 +4,10 @@
 
 ?>
 
-<?php
-
-
-
- ?>
-
-<!--
-key = 47e35c736a6e35721afced8e77d0c84b
-secret = 1f99a16a112aa373bf3af6d24a812690
-token = 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
-
-Private applications authenticate with Shopify through basic HTTP authentication, using the URL format
- https://{apikey}:{password}@{hostname}/admin/api/{version}/{resource}.json 
-
-
- https://47e35c736a6e35721afced8e77d0c84b:{password}@{hostname}/admin/api/{version}/{resource}.json 
-
-
- https://466d9e3095d01cf9926271dfe981eb02:f46553416d5e3e99c3e8b41f55e59694@humidors-direct.myshopify.com/admin/api/2020-01/products.json
- https://53d8b292ac04af188fe0e5042f590e2a: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@humidors-direct.myshopify.com/admin/api/2022-04/orders.json
-
--->
-
 
 <?php
-  //echo "test";
+
   /*
-
-  $url = "https://53d8b292ac04af188fe0e5042f590e2a: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@humidors-direct.myshopify.com/admin/api/2022-04/orders.json";
-
-  $curl = curl_init($url);
-  curl_setopt($curl, CURLOPT_URL, $url);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-  $headers = array(
-    "Accept: application/json",
-  );
-  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-  //for debug only!
-  //curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-  //curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-  $resp = curl_exec($curl);
-  curl_close($curl);
-
-  //var_dump($resp);
-  //print_r($resp['id']);
-  */
-
-?>
-
-
-
-<?php
-/*
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "apitest";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO `api_data`(`id`, `order_id`, `checkout_id`, `order_details`, `order_data`) VALUES ('','','','','')";
-
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-*/
-?> 
-
-
-  <!-- 'host' => 'localhost',
-  'dbname' => 'honeystore',
-  'username' => 'user_honeystore',
-  'password' => 'WW4)ski$@@12py', -->
-
-
-
-<?php
-
     $servername = "localhost";
     $username = "root";
     $password = "123";
@@ -106,6 +20,7 @@ $conn->close();
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
+    */
 
     echo "Shopify Order Details</br>";
 
@@ -190,7 +105,7 @@ $conn->close();
           echo "not call the file";
         }
         */
-
+        /*
         if(!empty($id)){
           $curl = curl_init();
           $data = array('id'=>$id,'checkout_id'=> $checkout_id, 'order_number' => $order_number); // Add parameters in key value
@@ -219,6 +134,7 @@ $conn->close();
         }else{
           echo "not call 2nd file";
         }
+        */
 
 
 
@@ -229,32 +145,9 @@ $conn->close();
         $message .= "</table>";
         $message .= "</body></html>";
         echo $message;
-        $conn->close();
+        //$conn->close();
         
         
         
     ?>
 
-
-<!--
-
-/*
-            if ($conn->query($sql) === TRUE) {
-              echo "New record created successfully"."</br>";
-            } else {
-              echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-            */
-
-/*
-  if($varified){
-    $response = $data;
-  }else{
-    $response = "Something wrong";
-  }
-
-  $log = fopen('ordernew.json', 'w') or die ('can not open the file');
-  fwrite($log, $response);
-  fclose($log);
-  */
-  -->
